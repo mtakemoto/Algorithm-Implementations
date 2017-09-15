c960fd78e111445e576a31b40b50ef16cace5c67
Matt Takemoto <matt.t.takemoto@gmail.com>
2017-09-14 17:48:57 -1000

    Revising project structure

---
+++ test/main/MergeSortTest.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author doyou
+ */
+public class MergeSortTest {
+    
+    public MergeSortTest() {
+        
+    }
+    
+    @Test
+    public void testNullInput() {
+        System.out.println("Null input");
+        MergeSort.Sort(null, 0, 0);
+    }
+    
+    @Test
+    public void testSingleElement() {
+        System.out.println("Single element sort");
+        int[] initial = {1};
+        int[] expected = {1};
+        MergeSort.Sort(initial, 0, initial.length-1);
+        assertArrayEquals(initial, expected);
+    }
+    
+    @Test
+    public void testSortOdd() {
+        System.out.println("Odd number of elements");
+        int[] initial = {10, 7, 9, 6, 8};
+        int[] expected = {6, 7, 8, 9, 10};
+        MergeSort.Sort(initial, 0, initial.length-1);
+        assertArrayEquals(initial, expected);
+    }
+    
+    @Test
+    public void testSortEven() {
+        System.out.println("Even number of elements");
+        int[] initial = {4, 2, 1, 3};
+        int[] expected = {1, 2, 3, 4};
+        MergeSort.Sort(initial, 0, initial.length-1);
+        assertArrayEquals(initial, expected); 
+    }
+    
+    @Test
+    public void testAllEqualNumbers() {
+        System.out.println("All elements are equal");
+        int[] initial = {1, 1, 1};
+        int[] expected = {1, 1, 1};
+        MergeSort.Sort(initial, 0, initial.length-1);
+        assertArrayEquals(initial, expected);
+    }
+    
+    @Test
+    public void testDuplicateNumbers() {
+        System.out.println("Handles duplicate elements");
+        int[] initial = {2, 2, 1, 3, 4, 4, 5};
+        int[] expected = {1, 2, 2, 3, 4, 4, 5};
+        MergeSort.Sort(initial, 0, initial.length-1);
+        assertArrayEquals(initial, expected);
+    }
+}
