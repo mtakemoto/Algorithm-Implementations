8c5fa56867cbb0135f69285bf7244f210e4bdaab
Matt Takemoto <matt.t.takemoto@gmail.com>
2017-09-19 15:47:24 -1000

    Null array test for linkedList

--- src/test/java/linkedLists/LinkedListTest.java
+++ src/test/java/linkedLists/LinkedListTest.java
@@ -5,9 +5,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LinkedListTest {
+    int[] emptyArray = new int[] {};
     int[] singleNumber = new int[] {1};
     int[] fiveNumbers = new int[] {1,2,3,4,5};
     int[] reversedNumbers = new int[] {5,4,3,2,1};
+
+    @Test
+    public void willInitializeEmpty() {
+        LinkedList list = new LinkedList(emptyArray);
+        assertEquals(list.get(), null);
+        System.out.println("Linked List handles empty array input");
+    }
 
     @Test
     public void willInitializeSingle() {
