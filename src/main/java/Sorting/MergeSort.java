57e47a99f05341bbe404d183f533bcc2c7ee3762
Matt Takemoto <matt.t.takemoto@gmail.com>
2017-09-19 15:28:13 -1000

    Reorganizing MergeSort into sorting package

---
+++ src/main/java/Sorting/MergeSort.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sorting;
+
+public class MergeSort {
+    
+    public static void Sort(int[] A, int low, int high) {
+        if(A == null) return;
+        if(low < high) {
+            int mid = (int)Math.floor((low + high) / 2);
+            Sort(A, low, mid);
+            Sort(A, mid+1, high);
+            Merge(A, low, mid, high);
+        }
+    }
+    
+    private static void Merge(int[] A, int low, int mid, int high) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+        int L[] = new int[n1 + 1];
+        int R[] = new int[n2 + 1];
+        
+        L[n1] = Integer.MAX_VALUE;
+        R[n2] = Integer.MAX_VALUE;
+        
+        for(int i=0; i < n1; i++) {
+            L[i] = A[low + i];
+        }
+        for(int j=0; j < n2; j++) {
+            R[j] = A[mid + j + 1];
+        }
+        int i = 0;
+        int j = 0;
+        for(int k=low; k<=high; k++) {
+            if(L[i] <= R[j]) {
+                A[k] = L[i];
+                i++;
+            }
+            else {
+                A[k] = R[j];
+                j++;
+            }
+        }
+    }
+}
